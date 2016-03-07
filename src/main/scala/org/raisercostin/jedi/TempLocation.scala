daaeb83c4fc681406669408ed7b9fb1b3deaa58c
raisercostin <raisercostin@gmail.com>
2016-03-07 14:21:17 +0100

    create random child in temp folder

--- src/main/scala/org/raisercostin/jedi/TempLocation.scala
+++ src/main/scala/org/raisercostin/jedi/TempLocation.scala
@@ -1,9 +1,9 @@
 package org.raisercostin.jedi
 
 import java.io.File
-
 import scala.language.implicitConversions
 import scala.language.reflectiveCalls
+import java.nio.file.Files
 
 
 case class TempLocation(temp: File, append: Boolean = false) extends FileLocationLike {self=>
@@ -11,6 +11,7 @@ case class TempLocation(temp: File, append: Boolean = false) extends FileLocatio
   override def withAppend: self.type = this.copy(append = true)
   def fileFullPath: String = temp.getAbsolutePath()
   def randomChild(prefix: String = "random", suffix: String = "") = new TempLocation(File.createTempFile(prefix, suffix, toFile))
+  def randomFolderChild(prefix: String = "random") = new TempLocation(Files.createTempDirectory(prefix).toFile)
   override def parent: Repr = new TempLocation(new File(parentName))
   override def child(child: String): Repr = new TempLocation(toPath.resolve(checkedChild(child)).toFile)
 }
