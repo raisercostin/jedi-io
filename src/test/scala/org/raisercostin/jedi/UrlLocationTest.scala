5e54e3c24851af1afdba9ff4e11ddcc09f7117c1
raisercostin <raisercostin@gmail.com>
2017-07-01 14:27:06 +0300

    fix test dependent on time

--- src/test/scala/org/raisercostin/jedi/UrlLocationTest.scala
+++ src/test/scala/org/raisercostin/jedi/UrlLocationTest.scala
@@ -143,8 +143,8 @@ class UrlLocationTest extends FunSuite with BaseLocationTest {
     val url = """https://commons.apache.org/proper/commons-io/javadocs/api-2.5/index.html"""
     println(Locations.url(url).meta.toString)
     val src = Locations.url(url)
-    val map1 = src.meta.get.asMap.mapValues(x=>x.toList).-("response.Keep-Alive").toSortedMap.mkString("\n")
-    val map2 = HierarchicalMultimap(src.metaLocation.flatMap(_.readContentAsText).get).asMap.mapValues(x=>x.toList).-("response.Keep-Alive").toSortedMap.mkString("\n")
+    val map1 = src.meta.get.asMap.mapValues(x=>x.toList).-("response.Keep-Alive").-("response.ETag").toSortedMap.mkString("\n")
+    val map2 = HierarchicalMultimap(src.metaLocation.flatMap(_.readContentAsText).get).asMap.mapValues(x=>x.toList).-("response.Keep-Alive").-("response.ETag").toSortedMap.mkString("\n")
     println("map1: " + map1)
     println("map2: " + map2)
     map1 shouldBe map2
