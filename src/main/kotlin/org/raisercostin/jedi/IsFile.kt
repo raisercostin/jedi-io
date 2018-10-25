bba8da228f607ef8d0b9e320a279caef6f6f7b2b
raisercostin <raisercostin@gmail.com>
2018-10-26 00:54:05 +0300

    fix

--- src/main/kotlin/org/raisercostin/jedi/IsFile.kt
+++ src/main/kotlin/org/raisercostin/jedi/IsFile.kt
@@ -1,5 +1,6 @@
 package org.raisercostin.jedi
+
 interface IsFile : IsFileOrFolder {
-  override fun isFile ()= true
-  override fun isFolder ()= false
+  override fun isFile(): Boolean = true
+  override fun isFolder(): Boolean = false
 }
--- src/main/kotlin/org/raisercostin/jedi/impl/ApacheFileUtils.kt
+++ src/main/kotlin/org/raisercostin/jedi/impl/ApacheFileUtils.kt
@@ -1,20 +1,20 @@
 package org.raisercostin.jedi.impl
 
 import org.apache.commons.io.FileUtils
-import java.nio.file.Files
+import java.io.File
 import java.io.IOException
-import java.nio.file.Paths
+import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.Paths
 
 object ApacheFileUtils {
-  fun forceDelete(path: Path) = try {
-    FileUtils.forceDelete(path.toFile)
-  } catch {
-    e: IOException ->
-      val msg = "Unable to delete file: "
-      if (e.getMessage.startsWith(msg)) {
-        Files.deleteIfExists(Paths.get(e.getMessage.stripPrefix(msg)))
-      } else
-        throw e
+  fun forceDelete(path: Path): Unit = try {
+    FileUtils.forceDelete(path.toFile())
+  } catch (e: IOException) {
+    val msg: String = "Unable to delete file: "
+    if (e.message!=null && e.message!!.startsWith(msg)) {
+        val deleteIfExists = Files.deleteIfExists(Paths.get(e.message!!.removePrefix(msg)))
+    } else /* ERROR converting `throw e`*/
+      throw e
   }
 }
