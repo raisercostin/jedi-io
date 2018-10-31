181a650a4f6ec705c5a07aca91f0103c102d11db
raisercostin <raisercostin@gmail.com>
2018-10-31 03:40:39 +0200

    cleanup traversal. and good tests

--- src/test/scala/org/raisercostin/jedi/JavaFileIteratorTest.java
+++ src/test/scala/org/raisercostin/jedi/JavaFileIteratorTest.java
@@ -3,14 +3,18 @@ package org.raisercostin.jedi;
 import static org.junit.Assert.*;
 
 import io.reactivex.Flowable;
+import io.reactivex.Single;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.raisercostin.jedi.Locations;
 
+import java.util.List;
+
 public class JavaFileIteratorTest {
     @Test
-    public void test() {
+    public void testNoPruning() {
         //Locations.file("d:\\home\\raiser\\work\\export-all\\revomatico\\all\\design\\").visit().take(3).forEach(System.out::println);
-        Flowable<?> res = Locations.current("src/test/resources").visit(
+        Flowable<?> res = Locations.current("src/test/resources").traverse(
                 Filters.anyFilterNoPruning("glob:**/*.{ZIP,jpg}", false));
         res.forEach(System.out::println);
         assertEquals(3, res.toList().blockingGet().size());
@@ -19,9 +23,9 @@ public class JavaFileIteratorTest {
     }
 
     @Test
-    public void test2() {
+    public void testGlobFilters() {
         Flowable<?> res = Locations.current("src/test/resources")
-                .visit(Filters.anyFilter("glob:**/*.{zip,jpg}", "glob:**/{folder,.git,.svn}", false));
+                .traverse(Filters.anyFilter("glob:**/*.{zip,jpg}", "glob:**/{folder,.git,.svn}", false));
         res.forEach(System.out::println);
         assertEquals(2, res.toList().blockingGet().size());
     }
@@ -29,20 +33,36 @@ public class JavaFileIteratorTest {
     @Test
     public void testIgnoreFolders() {
         Flowable<?> res = Locations.current("src/test/resources")
-                .visit(Filters.filter(Filters.createGlob("**/*.{zip,jpg}"),Filters.createGitFilter("#test\nfolder",true),false));
+                .traverse(Filters.filter(Filters.createGlob("**/*.{zip,jpg}"),Filters.createGitFilter("#test\nfolder",true),false));
         res.forEach(System.out::println);
         assertEquals(2, res.toList().blockingGet().size());
-        Flowable<?> res3 = Locations.file("d:\\home\\raiser\\work\\namek-uniboard\\")
-                .visit(Filters.filter(
-                        Filters.createGlob("**/*.{js,jpg}"),Filters.createGitFilter("#test\n.nuxt\nnode_modules",true),true));
-        res3.forEach(System.out::println);
     }
 
     @Test
     public void testSearchLikeInTotalCommander() {
         Flowable<?> res = Locations.current("")
-                .visit(Filters.createTotalCommanderFilter("*.zip *.jpg|folder"));
+                .traverse(Filters.createTotalCommanderFilter("*.zip *.jpg|folder"));
         res.forEach(System.out::println);
         assertEquals(33, res.toList().blockingGet().size());
+    }
+
+    @Test
+    @Ignore
+    public void checkBigFile() {
+        Flowable<?> res = Locations.file("d:\\home\\raiser\\work\\namek-uniboard\\")
+                .traverse(Filters.filter(
+                        Filters.createGlob("**/*.{js,jpg}"),Filters.createGitFilter("#test\n.nuxt\nnode_modules",true),true));
+        res.forEach(System.out::println);
+        assertEquals(15, res.toList().blockingGet().size());
+    }
+
+    @Test
+    @Ignore
+    public void checkSecondBigFile() {
+        Flowable<?> res = Locations.file("z:\\media\\media-ebooks2")
+                .traverse(Filters.filter(
+                        Filters.createGlob("**/*.zip"),Filters.createGitFilter("#test\n.git\nnode_modules",true),true)).cache();
+        res.forEach(System.out::println);
+        assertEquals(1980, res.toList().blockingGet().size());
     }
 }
